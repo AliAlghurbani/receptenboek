4ed14cec244a3ef0e8e0b9873c799dcb8c2d8d75
Ali <alimohalghurbani@gmail.com>
2023-03-23 12:39:08 +0100

    commit 1

--- site/svg/index.php
+++ site/svg/index.php
@@ -3,3 +3,5 @@
 // Dit is het startpunt van je applicatie.
 
 echo 'Welkom bij het receptenboek';
+
+?>
---
+++ site/website.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="website.css">
+    <title>Cubano</title>
+</head>
+
+<body>
+    <header>
+        <img id="logopic" src="LOGO-removebg-preview.png" alt="LOGO">
+        <nav>
+
+            <ul class="ulnav">
+                <li class="linav"> <a href="#">CUBA </a></li>
+                <li class="linav"><a href="#"> RECIPES </a></li>
+                <li class="linav"><a href="#"> SPECIAL EVENTS </a></li>
+                <li class="linav"><a href="#"> ABOUT OUR BOOK</a></li>
+            </ul>
+        </nav>
+        <div>
+            <ul class="ulsocials">
+                <li class="lisocials"><a href="#"> Facebook </a></li>
+                <li class="lisocials"><a href="#"> Youtube </a></li>
+                <li class="lisocials"><a href="#"> TikTok </a></li>
+                <li class="lisocials"><a href="#"> Twitter</a></li>
+                <li class="lisocials"><a href="#"> Instagram </a></li>
+            </ul>
+        </div>
+    </header>
+
+</body>
+
+</html>
