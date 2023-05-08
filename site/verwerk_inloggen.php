564aca9149974619672a5f9993c22ab062c590e7
Ali <alimohalghurbani@gmail.com>
2023-05-08 10:40:12 +0200

    commit3

--- site/verwerk_inloggen.php
+++ site/verwerk_inloggen.php
@@ -1,6 +1,5 @@
 <?php
 
-require 'database.php';
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
@@ -9,13 +8,26 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
+if (isset($_POST['email'])) {
+    //We controleren of de key email in de POST-array bestaat
 
-$email = $_POST['txtEmail'];
-$password = $_POST['txtPassword'];
+    //daarna controleren we of het emailadres is ingevuld (dus niet leeg)
+    if (!empty($_POST['email'])){
+        // dan komt hier de rest van de code.....
 
-$sql = "INSERT INTO gebruikers ( email, password)
- VALUES ( '$email', '$password' ) ";
+        //het eerste input field met een name attribuut 'email'
+        $email = $_POST ['email'];
 
-mysqli_query($conn, $sql);
+        //we hebben een database connicte nodig
+        require 'database.php';
+        //we gaan nu het emailadres dat is ingevuld vergelijken met de waardes in de database.
 
-?>
+        $sql = "SELECT * FROM gebruikers WHERE email = '$email'"; // check de single-qoutes naast email
+
+        //dan voeren we de query uit:
+        $result = mysqli_query($conn,$sql);
+
+        $user = mysqli_fetch_assoc($result);
+        
+    }
+}
